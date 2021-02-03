44e0f3a4c976d252a53b58624eb498c91d99350c
Jahid Hasan <slniloy500@gmail.com>
2021-02-03 08:11:31 +0600

    ES6

--- class.js
+++ class.js
@@ -1,31 +1,36 @@
-// class students{
-//     constructor(sId,sName){
-//         this.id = sId
-//         this.name = sName
-//     }
-// }
+class students{
+    constructor(sId,sName){
+        this.id = sId
+        this.name = sName
+        this.school = "Bashtoil Ideal School"
+    }
+}
 
-// const boy = new students(101,"Jahid");
+const boy = new students(101,"Jahid");
 
-// const girl = new students(105,"Hasan");
+const girl = new students(105,"Hasan");
 
 
-// console.log(boy,girl);
+console.log(boy,girl);
 
+
+
+/*
 const sName = ['jahid','hasan','Apple']
 
 for (let i = 0; i < sName.length; i++) {
     const student = sName[i];
     const sId = Math.random()*10000;
     const sIdNumber = Math.round(sId)
-    class students{
+    class Students{
         constructor(id,sNam){
             this.id = id;
             this.nam = sNam
         }
     }
-    const studentList = new students(sIdNumber,student);
+    const studentList = new Students(sIdNumber,student);
     console.log(studentList);
     
 }
 
+*/
---
+++ destructure.js
@@ -0,0 +1,4 @@
+const person = {name: 'jahid',age:17,job:'web developer',phone:"01794067954" , friends:['sojib','raznur','niloy']}
+
+const {name,phone} = person;
+console.log(name,phone);
